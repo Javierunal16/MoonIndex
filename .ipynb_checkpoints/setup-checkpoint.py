2378e40aaeb4451adbfbe3ac4b824184dee2d8e4
Javierunal16 <jaesuarezva@unal.edu.co>
2023-10-24 12:00:51 +0000

    Commit 1.0

--- .ipynb_checkpoints/setup-checkpoint.py
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -1,19 +1,22 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as f:
+    long_description =f.read()
+
 setup(
     name='MoonIndex',
     version='1.0',
     packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Javier Suarez',
     author_email='jsuarezvalencia@constructor.university',
     license='GPL',
     install_requires=['astropy==5.3.4'
+    , 'jmespath==1.0.1'                  
     , 'matplotlib==3.8.0'
-    , 'matplotlib-inline==0.1.6'
     , 'numpy==1.26.1'
     , 'opencv-python==4.8.1.78'
-    , 'plotly==5.17.0'
-    , 'pyproj==3.6.1'
     , 'pysptools==0.15.0'
     , 'python-dateutil==2.8.2'
     , 'rasterio==1.3.9'
