a9a17201cad5234e0af548332843fbd4cce9b70a
Javierunal16 <jaesuarezva@unal.edu.co>
2025-01-09 12:07:20 +0100

    Commit 2.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='MoonIndex',
-    version='2.0.3',
+    version='2.0.4',
     packages=find_packages(),
     long_description=long_description,
     long_description_content_type='text/markdown',
